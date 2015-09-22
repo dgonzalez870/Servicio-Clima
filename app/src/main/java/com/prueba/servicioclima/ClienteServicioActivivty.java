bcceb8bb9731e7adaee74cc5ea5b49bdf385bb1d
Dennys Gonzalez <dgonzalez870@yahoo.com>
2015-09-22 05:34:16 -0430

    Primer Commit con estructura del proyecto

---
+++ app/src/main/java/com/prueba/servicioclima/ClienteServicioActivivty.java
@@ -0,0 +1,6 @@
+package com.prueba.servicioclima;
+
+import android.app.Activity;
+
+public class ClienteServicioActivivty extends Activity {
+}
---
+++ app/src/main/java/com/prueba/servicioclima/ServicioClima.java
@@ -0,0 +1,11 @@
+package com.prueba.servicioclima;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ServicioClima extends IntentService {
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
